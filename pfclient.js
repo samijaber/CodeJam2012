8c481ca1ef4f9fd96c7ca3ec36f4f5b4aaabb0a5
lilred <carl.paten@gmail.com>
2012-11-17 18:38:20 -0500

    Added buy and sell TCP communication

--- pfclient.js
+++ pfclient.js
@@ -1,16 +1,25 @@
 "use strict";
 
 var _ = require('underscore');
+var net = require('net');
 
-var prices = [];
-var exchangeOpen = false;
+var pricefeed = [];
 
-var pfclient = require('net').connect({port: 8000},
+function logbuy() {
+	console.log("buy");
+}
+
+function logsell() {
+	console.log("sell");
+}
+
+var pfclient = net.connect({port: 8000},
     function() { //'connect' listener
   console.log('pfclient connected');
-  exchangeOpen = true;
   pfclient.write('H\n');
 });
+
+pfclient.setEncoding('ascii');
 
 pfclient.on('data', function(data) {
 
@@ -19,6 +28,7 @@ pfclient.on('data', function(data) {
 		.split("|");
 
 	if(arr.pop() == "C") { //Empty string, except when it's C, then it means the exchange closed
+		bsclient.end();
 		pfclient.end();
 	}
 
@@ -27,14 +37,20 @@ pfclient.on('data', function(data) {
 	;
 
 	_.map(arr, function(val) {
-		prices.push(val);
-		SMA(_.last(prices, 20));
+		pricefeed.push(val);
+		SMA(_.last(pricefeed, 20));
 	});
+
+	xOver(
+		_.last(SMA_5, arr.length), 
+		_.last(SMA_20, arr.length), 
+		bsclient.buy,
+		bsclient.sell
+	);
 });
 
 pfclient.on('end', function() {
-	exchangeOpen = false;
-	console.log('client disconnected');
+	console.log('pfclient disconnected');
 });
 
 
@@ -76,7 +92,7 @@ function SMA(dp20)
 
 function SMA_optimize(datapoints, n)
 {
-	return _.last(datapoints)/n - datapoints[datapoints.length-n-1];
+	return (_.last(datapoints) - datapoints[datapoints.length-n-1])/n;
 }
 
 //sum function for array x
@@ -84,3 +100,74 @@ function sum(x) {
 	//code from Underscore.js Docmuentation
 	return _.reduce(x, function(memo, num){return memo+num;},0);
 }
+
+var fastAboveSlow; //Initialize to the right value!
+
+function xOverArray(fastarr, slowarr, buy, sell) {
+	if(fastarr.length != slowarr.length) {
+		throw "length of the fast and slow datasets are different";
+	}
+
+	if(typeof fastAboveSlow === 'undefined') {
+		fastAboveSlow = fastarr[0] > slowarr[0];
+	}
+
+	_.map(_.zip(fastarr, slowarr), function(arr) {
+		xOverHelper(arr[0], arr[1], buy, sell);
+	});
+}
+
+function xOver(fast, slow, buy, sell) {
+	if(typeof fastAboveSlow === 'undefined') {
+		fastAboveSlow = fast > slow;
+		return;
+	}
+
+	xOverHelper(fast, slow, buy, sell);
+}
+
+function xOverHelper(fast, slow, buy, sell) {
+	if(fastAboveSlow != fast > slow) {
+		fastAboveSlow = !fastAboveSlow;
+		if(fastAboveSlow) { //If fast just overtook slow
+			buy();
+		} else {
+			sell();
+		}
+	}
+}
+
+var bsclient = net.connect({port: 9000});
+
+bsclient.setEncoding('ascii');
+
+var modes = [],
+	bsprices = [];
+
+bsclient.buy = function() {
+	modes.push("B");
+	bsclient.write('B\n');
+}
+
+bsclient.sell = function() {
+	modes.push("S");
+	bsclient.write('S\n');
+}
+
+bsclient.on('data', function(data) {
+	bsprices.push(data);
+});
+
+bsclient.on('end', function() {
+	var pricesfmt = [];
+	_.map(bsprices, function(price) {
+		try {
+			pricesfmt.push(price.toFixed(3));
+		} catch (err) {
+			pricesfmt = pricesfmt.concat(price.toString().match(/\d+\.\d{3}/g));
+		}
+	});
+	pricesfmt = _.compact(pricesfmt).length;
+	console.log(modes.length);
+	_.first(modes, pricesfmt.length);
+});
