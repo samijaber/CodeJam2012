346e84c2ab140da30d1b24a68a8879a0f75365e4
lilred <carl.paten@gmail.com>
2012-11-17 12:14:40 -0500

    Added price struct

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
-var net = require('net');
-var client = net.connect({port: 8000},
+prices = [];
+
+var client = require('net').connect({port: 8000},
     function() { //'connect' listener
   console.log('client connected');
   client.write('H\n'); //possible 'H\n'
@@ -7,13 +8,12 @@ var client = net.connect({port: 8000},
 
 client.on('data', function(data) {
 
-	arr = data
+	var arr = data
 		.toString()
 		.replace(/\./gi, "")
 		.split("|");
 
-	endstr = arr.pop(); //Last element of this array is always an empty string...
-	if(endstr == "C") { //Except when it's C, then it means the exchange closed
+	if(arr.pop() == "C") { //Except when it's C, then it means the exchange closed
 		console.log("Exchange closed");
 	}
 
@@ -21,11 +21,13 @@ client.on('data', function(data) {
 		.map(function(a) {return parseInt(a);})
 	;
 
+	prices = prices.concat(arr);
+
 	//Add arr to buffer
 	//Throw event
-	client.end();
 });
 
 client.on('end', function() {
   console.log('client disconnected');
 });
+
