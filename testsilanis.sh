cea800f89dea1b0d3e320078be727d31163b0840
Sami Jaber <jabersami@gmail.com>
2013-02-26 14:40:30 -0500

    Removed test files

--- testsilanis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-# create signing process 
-curl -X "POST" -H "Authorization: Basic Y29kZWphbTpBRkxpdGw0TEEyQWQx" -H \
-"Content- Type:application/json" -w http_code --data-binary @codejam.json \
- "https://stage-api.e-signlive.com/aws/rest/services/codejam" > output.txt;
